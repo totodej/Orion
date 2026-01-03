14ac254c264d814dc0ef30371cfb982dc0f586f8
Thomas Dejardins <tdejardins@gmail.com>
2026-01-03 11:05:29 +0100

    add posts list page and responsive

--- front/src/app/pages/posts-list/posts-list.component.ts
+++ front/src/app/pages/posts-list/posts-list.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Post } from 'src/app/core/models/post';
+import { PostsService } from 'src/app/core/services/posts.service';
 
 @Component({
   selector: 'app-posts-list',
   templateUrl: './posts-list.component.html',
-  styleUrls: ['./posts-list.component.scss']
+  styleUrls: ['./posts-list.component.scss'],
 })
 export class PostsListComponent implements OnInit {
+  posts: Post[] = [];
+  sortDirection: 'asc' | 'desc' = 'desc';
 
-  constructor() { }
+  constructor(private postsService: PostsService, private router: Router) {}
 
   ngOnInit(): void {
+    this.loadPosts();
   }
 
+  openPost(postId: number): void {
+    this.router.navigate(['/post', postId]);
+  }
+
+  loadPosts(): void {
+    this.postsService.getAllPosts(this.sortDirection).subscribe({
+      next: (data) => {
+        this.posts = data;
+      },
+      error: (err) => {
+        console.error("Erreur lors du chargement des posts :", err);
+      },
+    });
+  }
+
+  toggleSort(): void {
+    this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    this.loadPosts();
+  }
 }
