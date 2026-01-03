42529720bbb313a8eb106d5c7f30ca685a7418bd
Thomas Dejardins <tdejardins@gmail.com>
2026-01-03 10:50:37 +0100

    add header component and responsive

--- front/src/app/components/header/header.component.ts
+++ front/src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter, map, Observable } from 'rxjs';
 import { AuthService } from 'src/app/core/services/auth.service';
@@ -6,40 +6,51 @@ import { AuthService } from 'src/app/core/services/auth.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  menuOpen = false;
+  isMobile = false;
 
   isLogged$: Observable<boolean>;
   showUserMenu = false;
 
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) { 
+  constructor(private authService: AuthService, private router: Router) {
     this.isLogged$ = this.authService.isLogged$;
   }
 
   ngOnInit(): void {
-    // On met à jour le bouton selon la route actuelle
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd),
-      map(() => this.router.url)
-    ).subscribe(url => {
-      this.showUserMenu = !!localStorage.getItem('token') 
-        && !['/login', '/register'].includes(url);
-    });
+    this.checkScreenSize();
 
-    // Écoute les changements d'état de connexion
-    this.isLogged$.subscribe(isLogged => {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => this.router.url)
+      )
+      .subscribe((url) => {
+        this.showUserMenu =
+          !!localStorage.getItem('token') &&
+          !['/login', '/register'].includes(url);
+      });
+
+    this.isLogged$.subscribe((isLogged) => {
       const currentUrl = this.router.url;
-      this.showUserMenu = isLogged && !['/login', '/register'].includes(currentUrl);
+      this.showUserMenu =
+        isLogged && !['/login', '/register'].includes(currentUrl);
     });
   }
 
   logout(): void {
     this.authService.logout();
-    this.router.navigate(['/'])
+    this.router.navigate(['/']);
   }
 
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  @HostListener('window:resize')
+  checkScreenSize(): void {
+    this.isMobile = window.innerWidth < 425;
+  }
 }
